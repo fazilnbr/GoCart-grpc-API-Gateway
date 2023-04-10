478dbf4dfa1f13cb0681a0db1832227869ab75cf
fazilnbr <fazilkp2000@gmail.com>
2023-04-10 10:23:21 +0530

    login
    Here complete login vrified user and next generate tocken

--- pkg/auth/routes/login.go
+++ pkg/auth/routes/login.go
@@ -6,26 +6,38 @@ import (
 
 	"github.com/fazilnbr/banking-grpc-microservice/pkg/auth/pb"
 	"github.com/fazilnbr/banking-grpc-microservice/pkg/domain"
+	"github.com/fazilnbr/banking-grpc-microservice/pkg/utils/response"
 	"github.com/gin-gonic/gin"
 )
 
 func Login(ctx *gin.Context, c pb.AuthServiceClient) {
 	b := domain.User{}
-
-	if err := ctx.BindJSON(&b); err != nil {
-		ctx.AbortWithError(http.StatusBadRequest, err)
+	err := ctx.BindJSON(&b)
+	if err != nil {
+		responses := response.ErrorResponse("Please give essencial data", err.Error(), nil)
+		ctx.Writer.Header().Set("Content-Type", "application/json")
+		ctx.Writer.WriteHeader(http.StatusBadRequest)
+		response.ResponseJSON(*ctx, responses)
 		return
 	}
-
 	res, err := c.Login(context.Background(), &pb.LoginRequest{
 		Email:    b.Email,
 		Password: b.Password,
+		Username: b.Username,
 	})
 
 	if err != nil {
-		ctx.AbortWithError(http.StatusBadGateway, err)
+		responses := response.ErrorResponse("Failed to Login user", err.Error(), nil)
+		ctx.Writer.Header().Set("Content-Type", "application/json")
+		ctx.Writer.WriteHeader(http.StatusBadRequest)
+		response.ResponseJSON(*ctx, responses)
 		return
 	}
 
-	ctx.JSON(http.StatusCreated, &res)
+	responses := response.SuccessResponse(true, "SUCCESS", res)
+	ctx.Writer.Header().Set("Content-Type", "application/json")
+	ctx.Writer.WriteHeader(http.StatusOK)
+	response.ResponseJSON(*ctx, responses)
+	return
+
 }
