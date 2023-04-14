d3c7e55e38b69bd62cf2ad19f1d9d6462e49c838
fazilnbr <fazilkp2000@gmail.com>
2023-04-14 19:57:53 +0530

    feat:add initserviceclient
    this function for to connect the product service

--- pkg/product/client.go
+++ pkg/product/client.go
@@ -1,2 +1,24 @@
 package product
 
+import (
+	"fmt"
+
+	"github.com/fazilnbr/GoCart-grpc-API-Gateway/pkg/auth/pb"
+	"github.com/fazilnbr/GoCart-grpc-API-Gateway/pkg/config"
+	"google.golang.org/grpc"
+)
+
+type ServiceClient struct {
+	Client pb.AuthServiceClient
+}
+
+func InitServiceClient(c *config.Config) pb.AuthServiceClient {
+	// using WithInsecure() because no SSL running
+	cc, err := grpc.Dial(c.ProductSvcUrl, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("Could not connect:", err)
+	}
+
+	return pb.NewAuthServiceClient(cc)
+}
