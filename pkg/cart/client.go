9483f1c74ebf9c66f4c4574928ccf9a29fffdf99
fazilnbr <fazilkp2000@gmail.com>
2023-04-15 14:56:16 +0530

    feat():connect cartserviceclient
    here config env and connlect to service client

--- pkg/cart/client.go
+++ pkg/cart/client.go
@@ -1,2 +1,24 @@
 package cart
 
+import (
+	"fmt"
+
+	"github.com/fazilnbr/GoCart-grpc-API-Gateway/pkg/cart/pb"
+	"github.com/fazilnbr/GoCart-grpc-API-Gateway/pkg/config"
+	"google.golang.org/grpc"
+)
+
+type ServiceClient struct {
+	Client pb.CartServiceClient
+}
+
+func InitServiceClient(c *config.Config) pb.CartServiceClient {
+	// using WithInsecure() because no SSL running
+	cc, err := grpc.Dial(c.CartSvcUrl, grpc.WithInsecure())
+
+	if err != nil {
+		fmt.Println("Could not connect:", err)
+	}
+
+	return pb.NewCartServiceClient(cc)
+}
